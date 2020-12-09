39d16d29ffdde3c619d069b959d4c18a4b68d8eb
Jonatan Bruno <jonatanbrunotec@gmail.com>
2020-12-08 21:21:14 -0300

    Versao do dia 08/12/2020

--- server.js
+++ server.js
@@ -16,72 +16,92 @@ app.use(bodyParser.json())
 
 app.use('/static', express.static(__dirname + '/public'));
 
+//RESPONSAVEL PELA PORTA
 app.listen(3000);
 
 //esse bloco é disparado pelo enviar do formulario
 app.post('/cadUsuario', function(req,res){
     usuario.create({
         text:req.body.text,
-        emai:req.body.email,
+        email:req.body.email,
         password:req.body.password
     }).then(function(){
-        res.send('Cadastrado com Sucesso')
+        usuario.findAll().then(function(doadores){
+        res.render('formulario',{doador: doadores.map(pagamento => pagamento.toJSON())})
+    })
     }).catch(function(erro){
-        res.render('formulario')
+           res.render("Erro" +erro)
+    })
+    //res.send("Nome: "+req.body.nome + "<br>Valor: "+ req.body.senha)
+})
+
+
+
+//Rotas para o navegador
+app.get('/formulario', function(req,res){
+    usuario.findAll().then(function(doadores){
+    res.render('formulario', {doador: doadores.map(pagamento => pagamento.toJSON())})
+
     })
 })
 
 
-//Rotas para o navegador
-
-app.get('/formulario', function(req,res){
-    res.render('formulario')
-})
-
+//ROTAS DO SITE
 
 app.get('/', function(req,res){
     res.render('pagina-principal')
 })
 
-//Rotas para o site
+//---------------------------------------------------------------
+
 app.get('/missao', function(req,res){
     res.render('missao')
 })
+
+//---------------------------------------------------------------
 
 app.get('/valores', function(req,res){
     res.render('valores')
 })
 
+//---------------------------------------------------------------
+
 app.get('/abertura-de-empresa', function(req,res){
     res.render('abertura-de-empresa')
 })
+
+//---------------------------------------------------------------
 
 app.get('/folha-de-pagamento-e-declaracoes', function(req,res){
     res.render('folha-de-pagamento-e-declaracoes')
 })
 
+//---------------------------------------------------------------
+
 app.get('/apuracao-de-impostos-mensais-e-declaracoes', function(req,res){
     res.render('apuracao-de-impostos-mensais-e-declaracoes')
 })
+
+//---------------------------------------------------------------
 
 app.get('/contabilidade-societaria', function(req,res){
     res.render('contabilidade-societaria')
 })
 
+//---------------------------------------------------------------
+
 app.get('/contabilidade-consultiva', function(req,res){
     res.render('contabilidade-consultiva')
 })
+
+//---------------------------------------------------------------
 
 app.get('/area-restrita-do-cliente', function(req,res){
     res.render('area-restrita-do-cliente')
 })
 
+//---------------------------------------------------------------
+
 app.get('/sistema-web', function(req,res){
     res.render('sistema-web')
-})
-
-app.get('/login/meulogin', function(req,res){
-    usuario.findAll().then(function(doadores){
-        res.render('formulario', {doador: doadores.map(pagamento => pagamento.toJSON())})
-    })
 })
